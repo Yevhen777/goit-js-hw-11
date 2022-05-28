e9f1445f2115c1e8c6e2fde51c0bae2469dbf3c0
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-05-28 19:57:45 +0300

    hw - 11

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,25 +1,24 @@
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 
 let marginImgCard = '';
 let widthImages = '';
 
 const gallery = document.querySelector('.gallery');
 
-const lightbox = new SimpleLightbox('.gallery__item', 
-{captionsData: "alt", captionDelay: 250,});
-
+const lightbox = new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: 250 });
 
 function createMarkup(arrayForMarkup) {
-    if (gallery.clientWidth > 1100) {
-      marginImgCard = 5;  
-      widthImages = (gallery.clientWidth / 4) - (marginImgCard * 3);
-    } else {
-      marginImgCard = 5;  
-      widthImages = (gallery.clientWidth / 3) - (marginImgCard * 4);
-    }
-    
-    const markupTemplate = arrayForMarkup.map(el => {
-        return `<div class="photo-card" style=" margin:${marginImgCard}px">
+  if (gallery.clientWidth > 1100) {
+    marginImgCard = 5;
+    widthImages = gallery.clientWidth / 4 - marginImgCard * 3;
+  } else {
+    marginImgCard = 5;
+    widthImages = gallery.clientWidth / 3 - marginImgCard * 4;
+  }
+
+  const markupTemplate = arrayForMarkup
+    .map(el => {
+      return `<div class="photo-card" style=" margin:${marginImgCard}px">
         <a class="gallery__item" href="${el.largeImageURL}"><img src="${el.webformatURL}" class="img" alt="${el.tags}" loading="lazy" width="${widthImages}px"/></a>
         <div class="info" style="width:${widthImages}px">
           <p class="info-item">
@@ -39,12 +38,12 @@ function createMarkup(arrayForMarkup) {
             <span>${el.downloads}</span>
           </p>
         </div>
-        </div>`
-      }).join('');
-      
-      gallery.insertAdjacentHTML('beforeend', markupTemplate)
-      lightbox.refresh();  
-  }
+        </div>`;
+    })
+    .join('');
 
+  gallery.insertAdjacentHTML('beforeend', markupTemplate);
+  lightbox.refresh();
+}
 
-  export {createMarkup}
+export { createMarkup };
